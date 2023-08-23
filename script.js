4a4214f5fd8c4218151ac4de3228981a247b3ea2
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-08-23 12:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere(event) {
     //Write your code here
 	event.preventDefault();
 	var radius = parseFloat(document.getElementById("radius").value);
-	var Volume = (Math.floor(4/3) * Math.PI * Math.pow(radius, 3)).toFixed(2);
+	var Volume = ((4/3) * Math.PI * Math.pow(radius, 3)).toFixed(2);
 	var display = document.getElementById("volume");
 	display.value = Volume;
   
